102c844c56556ed39b6c2b6b769b0d576fe0d99e
Akhil kumar <94732080+akhiliano@users.noreply.github.com>
2021-11-21 09:41:46 +0530

    Add files via upload

---
+++ trail.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ 
+ int main(){
+    //  int a,b,c;
+    //  short _sa=9;
+    //  cout<<_sa<<endl;
+    //  cout<<"Hello world"<<endl;
+    //  float score=45.76;
+    //  cout<<"the score is "<<score;
+    int a,b;
+    cout<<"Enter first number"<<endl;
+    cin>>a;
+
+     cout<<"Enter second number"<<endl;
+    cin>>b;
+
+    cout<<"The sum is"<<a + b<<endl;
+    cout<<"The difference is"<<a - b<<endl;
+    cout<<"The multiplication is"<<a * b<<endl;
+    cout<<"The division is"<<(float) a / b<<endl;
+
+
+
+     return 0;
+ }
